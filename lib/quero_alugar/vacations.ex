4fd275ebbef4dfb85d0d5a3d549600572356078b
Diego Linhares <diegomlinhares@gmail.com>
2021-01-23 10:06:55 -0300

    Add Vacations

---
+++ lib/quero_alugar/vacations.ex
@@ -0,0 +1,93 @@
+defmodule QueroAlugar.Vacations do
+  import Ecto.Query
+
+  alias QueroAlugar.Repo
+
+  alias QueroAlugar.Vacations.{Place, Booking, Review}
+  alias QueroAlugar.Accounts.User
+
+  def get_place_by_slug!(slug) do
+    Repo.get_by!(Place, slug: slug)
+  end
+
+  def list_places do
+    Repo.all(Place)
+  end
+
+  def list_places(criteria) do
+    query = from(p in Place)
+
+    Enum.reduce(criteria, query, fn
+      {:limit, limit}, query ->
+        from p in query, limit: ^limit
+
+      {:filter, filters}, query ->
+        filter_with(filters, query)
+
+      {:order, order}, query ->
+        from p in query, order_by: [{^order, :id}]
+    end)
+    |> IO.inspect()
+    |> Repo.all()
+  end
+
+  defp filter_with(filters, query) do
+    Enum.reduce(filters, query, fn
+      {:matching, term}, query ->
+        pattern = "%#{term}%"
+
+        from q in query,
+          where:
+            ilike(q.name, ^pattern) or
+              ilike(q.description, ^pattern) or
+              ilike(q.location, ^pattern)
+
+      {:pet_friendly, value}, query ->
+        from q in query, where: q.pet_friendly == ^value
+
+      {:pool, value}, query ->
+        from q in query, where: q.pool == ^value
+
+      {:wifi, value}, query ->
+        from q in query, where: q.wifi == ^value
+
+      {:guests_count, count}, query ->
+        from q in query, where: q.max_guests >= ^count
+
+      {:available_between, %{start_date: start_date, end_date: end_date}}, query ->
+        available_between(query, start_date, end_date)
+    end)
+  end
+
+  defp available_between(query, start_date, end_date) do
+    from place in query,
+      left_join: booking in Booking,
+      on:
+        booking.place_id == place.id and
+          fragment(
+            "(?, ?) OVERLAPS (?, ? + INTERVAL '1' DAY",
+            booking.start_date,
+            booking.end_date,
+            type(^start_date, :date),
+            type(^end_date, :date)
+          ),
+      where: is_nil(booking.place_id)
+  end
+
+  def get_booking!(id) do
+    Repo.get!(Booking, id)
+  end
+
+  def create_booking(%User{} = user, attrs) do
+    %Booking{}
+    |> Booking.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
+    |> Repo.insert()
+  end
+
+  def cancel_booking(%Booking{} = booking) do
+    booking
+    |> Booking.cancel_changeset(%{state: "canceled"})
+    |> Repo.update()
+  end
+end
